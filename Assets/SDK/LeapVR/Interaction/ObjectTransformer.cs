e903f530ab27d19adda8a4de1b0c3f5dfce6d011
VoidX <voidxb@gmail.com>
2018-03-25 06:23:45 +0200

    Scaling support in the object transformer

--- Assets/SDK/LeapVR/Interaction/ObjectTransformer.cs
+++ Assets/SDK/LeapVR/Interaction/ObjectTransformer.cs
@@ -8,15 +8,65 @@ namespace LeapVR {
     public class ObjectTransformer : MonoBehaviour {
         public Transform Target;
 
+        [Header("Movement")]
+        public bool EnableMovement = true;
         [Tooltip("Sensitivity for movement (single hand grab).")]
-        public float MoveSensitivity = .1f;
+        public float MovementSensitivity = .1f;
+
+        [Header("Rotation")]
+        public bool EnableRotation = true;
         [Tooltip("Sensitivity for rotation (dual hand grab).")]
         public float RotationSensitivity = .5f;
+
+        [Header("Scaling")]
+        public bool EnableScaling = true;
+        [Tooltip("Sensitivity for scaling (dual hand pinch).")]
+        public float ScalingSensitivity = .005f;
+
+        void Start() {
+            MinScale = transform.localScale * .0001f;
+        }
+
+        void Update() {
+            if (LeapMouse.Instance.Action()) {
+                int LeftID = LeapMotion.Instance.FirstLeftHand(), RightID = LeapMotion.Instance.FirstRightHand();
+                if (LeftID != -1 && RightID != -1 && LeapMotion.Instance.ExtendedFingers(LeftID) == 0 && LeapMotion.Instance.ExtendedFingers(RightID) == 0) { // Rotate
+                    Vector3 Left = LeapMotion.Instance.PalmPosition(LeftID), Right = LeapMotion.Instance.PalmPosition(RightID), NewRotation = Right - Left;
+                    if (PrevRotation != Vector3.up) {
+                        Transform Cam = Camera.main.transform;
+                        Vector3 Delta = (NewRotation - PrevRotation) * RotationSensitivity;
+                        if (EnableRotation)
+                            Target.rotation = Quaternion.Euler(Cam.right * (Right.y - PrevHeight - Delta.y)) * Quaternion.Euler(Cam.forward * Delta.y) *
+                                Quaternion.Euler(Cam.up * Delta.z) * Target.rotation;
+                        if (EnableScaling)
+                            Target.localScale = Vector3.Max(Target.localScale * (1 + Delta.x * ScalingSensitivity), MinScale);
+                    }
+                    PrevHeight = Right.y;
+                    PrevRotation = NewRotation;
+                    Target.position = MovementStart;
+                } else { // Move
+                    Vector3 PalmPos = LeapMotion.Instance.PalmPosition();
+                    PalmPos = new Vector3(PalmPos.x, PalmPos.y, -PalmPos.z);
+                    if (EnableMovement && PrevPosition != Vector3.up)
+                        Target.position += Camera.main.transform.rotation * ((PalmPos - PrevPosition) * MovementSensitivity);
+                    PrevPosition = PalmPos;
+                }
+            } else {
+                PrevPosition = Vector3.up;
+                PrevRotation = Vector3.up;
+                MovementStart = Target.position;
+            }
+        }
+
+        /// <summary>
+        /// Previous hand height.
+        /// </summary>
+        float PrevHeight;
 
         /// <summary>
         /// Starting position of a movement, to cancel the movement if the gesture is a rotation.
         /// </summary>
-        Vector3 MoveStart;
+        Vector3 MovementStart;
 
         /// <summary>
         /// Previous hand position for movement.
@@ -28,31 +78,9 @@ namespace LeapVR {
         /// </summary>
         Vector3 PrevRotation;
 
-        void Update() {
-            if (LeapMouse.Instance.Action()) {
-                int LeftID = LeapMotion.Instance.FirstLeftHand(), RightID = LeapMotion.Instance.FirstRightHand();
-                if (LeftID != -1 && RightID != -1 && LeapMotion.Instance.ExtendedFingers(LeftID) == 0 && LeapMotion.Instance.ExtendedFingers(RightID) == 0) { // Rotate
-                    Vector3 Left = LeapMotion.Instance.PalmPosition(LeftID), Right = LeapMotion.Instance.PalmPosition(RightID), NewRotation = Right - Left;
-                    if (PrevRotation != Vector3.up) {
-                        Transform Cam = Camera.main.transform;
-                        Vector3 Delta = (NewRotation - PrevRotation) * RotationSensitivity;
-                        Target.rotation = Quaternion.Euler(Cam.right * Delta.x) * Quaternion.Euler(Cam.forward * Delta.y) *
-                            Quaternion.Euler(Cam.up * Delta.z) * Target.rotation;
-                    }
-                    PrevRotation = NewRotation;
-                    Target.position = MoveStart;
-                } else { // Move
-                    Vector3 PalmPos = LeapMotion.Instance.PalmPosition();
-                    PalmPos = new Vector3(PalmPos.x, PalmPos.y, -PalmPos.z);
-                    if (PrevPosition != Vector3.up)
-                        Target.position += Camera.main.transform.rotation * ((PalmPos - PrevPosition) * MoveSensitivity);
-                    PrevPosition = PalmPos;
-                }
-            } else {
-                PrevPosition = Vector3.up;
-                PrevRotation = Vector3.up;
-                MoveStart = Target.position;
-            }
-        }
+        /// <summary>
+        /// Minimal possible scaling.
+        /// </summary>
+        Vector3 MinScale;
     }
 }
